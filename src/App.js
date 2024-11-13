817c94013ad93622472e96fbdc5c7dff0af59bc7
arushpandey2006 <ggtxarush@gmail.com>
2024-11-13 17:56:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Otppage from "./component/Otppage"
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App d-flex justify-content-center align-items-center vh-100">
+    <Otppage/>
+    </div>
+  );
+}
+
+export default App;
